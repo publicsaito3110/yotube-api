fa14c09c5147c8324b87a7a6e0f68360758d9fe0
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-16 13:16:03 +0900

    first commit

--- src/test/java/com/yotubeAPI/YotubeAPIApplicationTests.java
+++ src/test/java/com/yotubeAPI/YotubeAPIApplicationTests.java
@@ -1,10 +1,10 @@
-package com.lyric;
+package com.yotubeAPI;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LyricApplicationTests {
+class YotubeAPIApplicationTests {
 
 	@Test
 	void contextLoads() {
