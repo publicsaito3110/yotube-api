0f78c567f953688afc98e154865b003e1cb7080f
publicsaito3110 <public.business.saito3110@gmail.com>
2022-08-16 13:11:57 +0900

    first commit

---
+++ src/main/java/com/yotubeAPI/YotubeAPIApplication.java
@@ -0,0 +1,16 @@
+package com.yotubeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author saito
+ *
+ */
+@SpringBootApplication
+public class YotubeAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YotubeAPIApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lyric/LyricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
